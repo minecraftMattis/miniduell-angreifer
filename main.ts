c43478b67f2e23c2a71039474152fbeb1b188e1a
minecraftMattis <mattis.lueger@icloud.com>
2025-10-03 12:04:23 +0200

    + funktion zeigeSymbol

--- main.ts
+++ main.ts
@@ -4,17 +4,9 @@ input.onButtonPressed(Button.A, function () {
     } else {
         _symbol = 2
     }
+    zeigeSymbol()
 })
-input.onButtonPressed(Button.B, function () {
-    if (_symbol < 2) {
-        _symbol = _symbol + 1
-    } else {
-        _symbol = 0
-    }
-})
-let _symbol = 0
-_symbol = 0
-basic.forever(function () {
+function zeigeSymbol () {
     if (_symbol == 0) {
         basic.showIcon(IconNames.Sword)
     } else if (_symbol == 1) {
@@ -30,4 +22,15 @@ basic.forever(function () {
     } else {
         basic.showIcon(IconNames.No)
     }
+}
+input.onButtonPressed(Button.B, function () {
+    if (_symbol < 2) {
+        _symbol = _symbol + 1
+    } else {
+        _symbol = 0
+    }
+    zeigeSymbol()
 })
+let _symbol = 0
+_symbol = 0
+zeigeSymbol()
